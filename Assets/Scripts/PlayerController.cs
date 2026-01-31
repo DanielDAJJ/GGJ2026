fb9c856c5dde0e2aa72033e45eb2694b5c325c84
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2026-01-31 18:19:36 -0500

    Mecanica de doble salto
    Se implementa mecánica de doble salto y salto desde la pared puliéndolo de la mejor manera posible

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,17 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float dashforce = 5f;
     [SerializeField] private float dashCooldown = 2f;
-    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpForce = 7f;
     [SerializeField] private float force = 10f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private Vector2 wallCheckSize = new Vector2(0.5f, 0f);
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private float groundCheckRadius = 0.3f;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float verticalInput;
     [SerializeField] private float jumpInput;
+    [SerializeField] private float extraJumps = 1f;
+    [SerializeField] private float reamingJumps;
     [SerializeField] private float dashInput;
+    [SerializeField] private float wallJumpTime;
     [Header("-----------------------States")]
     [SerializeField] private bool isAlive = true;
     [SerializeField] private bool isGrounded;
@@ -30,12 +32,16 @@ public class PlayerController : MonoBehaviour
     public enum maskStates {none, monkey, leopard, rhino}
     [SerializeField] private maskStates currentMask = maskStates.none;
     [Header("-----------------------References")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
 
 
     void Awake()
     {
+        groundLayer = LayerMask.GetMask("Ground");
+        wallLayer = LayerMask.GetMask("Wall");
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -51,14 +57,35 @@ public class PlayerController : MonoBehaviour
         if (!isAlive) return;
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        jumpInput = Input.GetButton("Jump") ? 1 : 0;
-        //dashInput = Input.GetButton("Shift") ? 1 : 0;
+        if (isGrounded || (currentMask == maskStates.monkey && isTouchingWall))
+        {
+            reamingJumps = extraJumps;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded || ( currentMask == maskStates.monkey && isTouchingWall))
+            {
+                Jump();
+            } else if(currentMask == maskStates.monkey && reamingJumps > 0)
+            {
+                Jump();
+                reamingJumps--;
+            }
+        }        
     }
     void FixedUpdate()
     {
         PlayerIsGrounded();
         PlayerIsTounchingWall();
-        MovePlayer(horizontalInput);
+        if(currentMask == maskStates.monkey && isTouchingWall)
+        {
+            ClimbWall();
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+            MovePlayer(horizontalInput);
+        }
     }
     private void OnDrawGizmos()
     {
@@ -72,26 +99,48 @@ public class PlayerController : MonoBehaviour
     }
     public void PlayerIsTounchingWall()
     {
-        isTouchingWall = Physics2D.OverlapBox(wallCheck.position, wallCheckSize, 0f, groundLayer);
+        isTouchingWall = Physics2D.OverlapBox(wallCheck.position, wallCheckSize, 0f, wallLayer);
     }
     public void MovePlayer(float horizontalInput)
     {
-        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
-        if (horizontalInput != 0)
+        if (wallJumpTime <= 0)
         {
-            transform.localScale = new Vector2(horizontalInput > 0 ? 1 : -1, 1);
+            rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+            if (horizontalInput != 0)
+            {
+                transform.localScale = new Vector2(horizontalInput > 0 ? 1 : -1, 1);
+            }
+        } 
+        else
+        {
+            wallJumpTime -= Time.deltaTime; 
         }
-        if (currentMask == maskStates.monkey) ClimbWall();
     }
     public void ClimbWall()
     {
-        if(isTouchingWall && verticalInput != 0)
+        if (isTouchingWall)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, verticalInput * moveSpeed);
-            rb.gravityScale = 0;
-        } else
+            rb.gravityScale = 0f;
+            float vSpeed = verticalInput * moveSpeed;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vSpeed);
+        }
+        else
         {
-            rb.gravityScale = 1;
+            rb.gravityScale = 1f;
+        }
+    }
+    public void Jump()
+    {
+        if(currentMask == maskStates.monkey && isTouchingWall && !isGrounded)
+        {
+            wallJumpTime = 0.2f;
+            float pushDirection = transform.localScale.x > 0 ? -1 : 1;
+            rb.linearVelocity = new Vector2(pushDirection * moveSpeed * 2f, jumpForce);
+            transform.localScale = new Vector2(pushDirection, 1);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
     public void Dash()
